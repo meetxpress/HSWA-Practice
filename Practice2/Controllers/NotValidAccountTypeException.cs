a002cb6901c9d0fd5c9134babfdf4dc7103291b3
meet patel <pmeet7895@gmail.com>
2021-05-13 22:50:37 +0530

    Second Commit

--- Practice2/Controllers/NotValidAccountTypeException.cs
+++ Practice2/Controllers/NotValidAccountTypeException.cs
@@ -4,16 +4,12 @@ using System.Runtime.Serialization;
 namespace Practice2.Controllers {
     [Serializable]
     internal class NotValidAccountTypeException : Exception {
-        public NotValidAccountTypeException() {
-        }
+        public NotValidAccountTypeException() { }
 
-        public NotValidAccountTypeException(string message) : base(message) {
-        }
+        public NotValidAccountTypeException(string message) : base(message) { }
 
-        public NotValidAccountTypeException(string message, Exception innerException) : base(message, innerException) {
-        }
+        public NotValidAccountTypeException(string message, Exception innerException) : base(message, innerException) { }
 
-        protected NotValidAccountTypeException(SerializationInfo info, StreamingContext context) : base(info, context) {
-        }
+        protected NotValidAccountTypeException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
